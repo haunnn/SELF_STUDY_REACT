36f29c83f0ef0741946442236556b3da78254b85
DESKTOP-DOGVPUB\SSAFY <ryuhaun0321@gmail.com>
2023-01-18 17:28:46 +0900

    0118 class

---
+++ login-test-2/src/App.js
@@ -0,0 +1,14 @@
+import Signin from './components/SignIn'
+import Login from './components/LogIn'
+
+function App() {
+  return (
+    <div>
+      <Signin/>
+      <hr/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
--- nomader_practice/src/App.js
+++ nomader_practice/src/App.js
@@ -1,33 +1,107 @@
+import React, { useState } from "react";
+
+function TimeInverter() {
+  const [amount, setAmount] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+
+  const onChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  return (
+    <>
+      <h2> Time Inverter </h2>
+      <div>
+        <label htmlFor="minute">Minute</label>
+        <input
+          id="minute"
+          value={flipped ? amount * 60 : amount}
+          onChange={onChange}
+          disabled={flipped}
+        />
+      </div>
+      <div>
+        <label htmlFor="hour">Hour</label>
+        <input
+          id="hour"
+          value={flipped ? amount : Math.round(amount / 60)}
+          onChange={onChange}
+          disabled={!flipped}
+        />
+      </div>
+
+      <button onClick={() => setAmount(0)}>Reset</button>
+      <button
+        onClick={() => {
+          setAmount(0);
+          setFlipped(!flipped);
+        }}
+      >
+        Flip
+      </button>
+    </>
+  );
+}
+
+function DistanceInverter() {
+  const [amount, setAmount] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+
+  const onChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  return (
+    <>
+      <h2> Distance Inverter </h2>
+      <div>
+        <label htmlFor="minute">Kilometer</label>
+        <input
+          id="minute"
+          value={flipped ? amount * 60 : amount}
+          onChange={onChange}
+          disabled={flipped}
+        />
+      </div>
+      <div>
+        <label htmlFor="hour">Mile</label>
+        <input
+          id="hour"
+          value={flipped ? amount : Math.round(amount / 60)}
+          onChange={onChange}
+          disabled={!flipped}
+        />
+      </div>
+
+      <button onClick={() => setAmount(0)}>Reset</button>
+      <button
+        onClick={() => {
+          setAmount(0);
+          setFlipped(!flipped);
+        }}
+      >
+        Flip
+      </button>
+    </>
+  );
+}
+
 function App() {
-  
-  function Header() {
-    return (
-      <h1>
-        Super Converter
-      </h1>
-    )
-  }
-  
-  function TimeInverter() {
-    return (
-
-    )
-  }
-
-  function DistanceInverter() {
-    return (
-      
-    )
-  }
+  const [index, setIndex] = useState("xx");
 
   return (
     <div>
-      <Header />
+      <h1>Super Converter</h1>
 
-      <TimeInverter />
+      <select value={index} onChange={(e) => setIndex(e.target.value)}>
+        <option value="xx">Select your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
 
-      <DistanceInverter />
-
+      {index === "0" ? <TimeInverter /> : null}
+      {index === "1" ? <DistanceInverter /> : null }
+    
     </div>
   );
 }
